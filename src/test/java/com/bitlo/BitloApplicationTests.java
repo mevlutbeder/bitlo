5a6a8386271e3d4aac6edf0baff4deb1181d77f7
mevlutbeder1 <Mev112233>
2021-09-18 15:11:38 +0300

    Initial commit

---
+++ src/test/java/com/bitlo/BitloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
